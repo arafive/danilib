abf552b4aa0c4e020cb8bf072623582fd305ff61
Daniele Carnevale <lacaselladidaniele@gmail.com>
2026-03-20 11:43:52 +0100

    Add f_crea_cartella

--- danilib/danilib.py
+++ danilib/danilib.py
@@ -1,3 +1,35 @@
 
+import os
+
 def f_buongiorno():
     print('Buongiorno :)')
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+def f_crea_cartella(percorso_cartella: str):
+    """
+    Crea una cartella, printa per conferma e ritorna il percorso passato.
+
+    Parameters
+    ----------
+    percorso_cartella : str
+        Percorso della cartella da creare.
+
+    Returns
+    -------
+    percorso_cartella : str
+        Percorso della cartella creata.
+
+    """
+    os.makedirs(percorso_cartella, exist_ok=True)
+    print(f'Creata cartella {percorso_cartella}')
+
+    return percorso_cartella
+
+    
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
